53d209a56e7afee570f09dde542c75d6b8aff26f
drmyers <drmyers@covermymeds.com>
2016-11-10 17:41:00 -0500

    Adding virtualbox at version 5.1.6.

---
+++ Casks/virtualbox-5.1.6.rb
@@ -0,0 +1,25 @@
+cask 'virtualbox' do
+  version '5.1.6-110634'
+  sha256 '3fc6e2f1eb6d25ccf91194a102d2dd22b51a23df469257ea4d3893e1c5056e85'
+
+  url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/VirtualBox-#{version}-OSX.dmg"
+  appcast 'http://download.virtualbox.org/virtualbox/LATEST.TXT',
+          checkpoint: '280bd9701a0fcbe1d7ef2e23ffede42d31db69bedaeb7b46084e450e653d8224'
+  name 'Oracle VirtualBox'
+  homepage 'https://www.virtualbox.org'
+
+  pkg 'VirtualBox.pkg'
+
+  uninstall script:  { executable: 'VirtualBox_Uninstall.tool', args: %w[--unattended] },
+            pkgutil: 'org.virtualbox.pkg.*'
+
+  zap delete: [
+                '/Library/Application Support/VirtualBox',
+                '~/Library/VirtualBox',
+                '~/Library/Preferences/org.virtualbox.app.VirtualBox.plist',
+                '~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist',
+                '~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState',
+                '~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState',
+              ],
+      rmdir:  '~/VirtualBox VMs'
+end
