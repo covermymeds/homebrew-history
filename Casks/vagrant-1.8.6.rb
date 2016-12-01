ea5aa9c31a690745fa0128e8155138a908996538
drm2 <davidrmyersii@gmail.com>
2016-12-01 14:10:52 -0500

    Updating sha256 checksum.

--- Casks/vagrant-1.8.6.rb
+++ Casks/vagrant-1.8.6.rb
@@ -1,6 +1,6 @@
 cask 'vagrant-1.8.6' do
   version '1.8.6'
-  sha256 :no_check # skipping check for now
+  sha256 'b916cd103c91faf57b63b49188e4cd09136f81385ff05d62e55b68c87b53a2d9'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}.dmg"
