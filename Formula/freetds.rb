1a49eb25a91dc0a6179d4bee62b90fda42ea4264
drm2 <davidrmyersii@gmail.com>
2017-01-16 15:46:34 -0500

    Add archive for FreeTDS at version 1.00.21.

---
+++ Formula/freetds.rb
@@ -0,0 +1,85 @@
+class Freetds < Formula
+  desc "Libraries to talk to Microsoft SQL Server and Sybase databases"
+  homepage "http://www.freetds.org/"
+  url "ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.00.21.tar.gz"
+  mirror "https://fossies.org/linux/privat/freetds-1.00.21.tar.gz"
+  sha256 "a163fbe8639657dd7c3add7be0231c5c76460a55b3925946933d8d4b424cc7ea"
+
+  bottle do
+    sha256 "436a77f9ce35fd4f1e83ab6ceede2fee46867711a0a2e6535a38117414933c32" => :sierra
+    sha256 "1b8cd6af7e27e079e6c06db542d12d02a2effe8b27d276925c627f057bae280f" => :el_capitan
+    sha256 "968cc5a0e4f39b45a93b4a082ef1ffd4cd6ef7719c7be2a0862e269c62536906" => :yosemite
+  end
+
+  head do
+    url "https://github.com/FreeTDS/freetds.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
+
+  option :universal
+  option "with-msdblib", "Enable Microsoft behavior in the DB-Library API where it diverges from Sybase's"
+  option "with-sybase-compat", "Enable close compatibility with Sybase's ABI, at the expense of other features"
+  option "with-odbc-wide", "Enable odbc wide, prevent unicode - MemoryError's"
+  option "with-krb5", "Enable Kerberos support"
+
+  deprecated_option "enable-msdblib" => "with-msdblib"
+  deprecated_option "enable-sybase-compat" => "with-sybase-compat"
+  deprecated_option "enable-odbc-wide" => "with-odbc-wide"
+  deprecated_option "enable-krb" => "with-krb5"
+
+  depends_on "pkg-config" => :build
+  depends_on "unixodbc" => :optional
+  depends_on "libiodbc" => :optional
+  depends_on "openssl" => :recommended
+
+  def install
+    if build.with?("unixodbc") && build.with?("libiodbc")
+      odie "freetds: --without-libiodbc must be specified when using --with-unixodbc"
+    end
+
+    args = %W[
+      --prefix=#{prefix}
+      --with-tdsver=7.3
+      --mandir=#{man}
+    ]
+
+    if build.with? "openssl"
+      args << "--with-openssl=#{Formula["openssl"].opt_prefix}"
+    end
+
+    if build.with? "unixodbc"
+      args << "--with-unixodbc=#{Formula["unixodbc"].opt_prefix}"
+    end
+
+    if build.with? "libiodbc"
+      args << "--with-libiodbc=#{Formula["libiodbc"].opt_prefix}"
+    end
+
+    # Translate formula's "--with" options to configuration script's "--enable"
+    # options
+    %w[msdblib sybase-compat odbc-wide krb5].each do |option|
+      if build.with? option
+        args << "--enable-#{option}"
+      end
+    end
+
+    ENV.universal_binary if build.universal?
+
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make"
+    ENV.j1 # Or fails to install on multi-core machines
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/tsql", "-C"
+  end
+end
